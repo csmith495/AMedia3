5919f7b8d89ceffcf8a8eba5b9b8b2392c509c52
csmith495 <tekF@ce1994>
2023-05-19 20:44:05 -0500

    Full code for exoplayer app

--- app/src/main/java/com/example/mymedia3/MainActivity.kt
+++ app/src/main/java/com/example/mymedia3/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.mymedia3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.PlayerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var playerView: PlayerView
+    lateinit var player: ExoPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playerView = findViewById(R.id.player_view)
+
+        val player = ExoPlayer.Builder(this).build()
+
+        playerView.player = player
+
+        val mediaItem = MediaItem.Builder()
+            .setUri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
+            .setMimeType(MimeTypes.APPLICATION_MP4)
+            .build()
+
+        val mediaSource = ProgressiveMediaSource.Factory(
+            DefaultDataSource.Factory(this)
+        ).createMediaSource(mediaItem)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
     }
 }
